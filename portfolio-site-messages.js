aac5ac0e1838350c68f69187557ea10387edddb3
Michael Wood <michaelwood33311@icloud.com>
2023-08-27 17:00:47 -0400

    added /message endpoint

--- portfolio-site-messages.js
+++ portfolio-site-messages.js
@@ -13,11 +13,26 @@ const mailgun = require('./utils/mailGun');
 const app = express();
 app.use(express.static('public'));
 app.use(express.json({limit: '200mb'})); 
+var corsOptions = {
+    origin: 'https://michaelcalvinwood.net',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
+
+const handleMessage = async (req, res) => {
+    const { name, email, message } = req.body;
+    if (!name || !email || !message) res.status(400).json('bad request');
+
+    const html = `Name: ${name}<br /><br />Email: ${email}<br><br>Message:<br />${message}`;
+    mailgun.sendEmailViaMailGun('michaelwood33311@icloud.com', 'admin@treepadcloud.com', 'Portfolio Site Message', html, 'TreePad Cloud');
+    return res.status(200).json('ok');
+}
+
+app.post('/message', (req, res) => handleMessage(req, res));
 
 const httpsServer = https.createServer({
     key: fs.readFileSync(privateKeyPath),
